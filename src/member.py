1beae60c1eb5c0336ab99664321b4b737505102c
JJ <40186744+tnfhrnsss@users.noreply.github.com>
2024-05-11 10:09:11 +0900

    #2 add try-catch

--- src/member.py
+++ src/member.py
@@ -12,9 +12,13 @@ def register(user_id, user_name):
         'X-Attic-Role': 'manageMember'
     }
     payload = {"userId": user_id, "name": user_name, "password": "1", "roleGroupId": "ADMIN", "centerIds":["spectra"], "customType": "user", "metadata": { "counselType": "user"}}
-    response = requests.post(api_url, headers=headers, json=payload)
-
-    if response.ok:
-        print("User {} registered successfully.".format(user_name))
-    else:
-        print("Failed to register user .{}.".format(user_name))
+    try:
+        response = requests.post(api_url, headers=headers, json=payload, timeout=5)
+        if response.ok:
+            print("User {} registered successfully.".format(user_name))
+        else:
+            print("Failed to register user .{}.".format(user_name))
+    except requests.exceptions.Timeout:
+        print("Timeout occurred.")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
